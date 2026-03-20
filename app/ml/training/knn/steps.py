79b5378fe1ea9c22090bf12700c85797e618520b
Nahuel Zalazar <zvnahuel63@gmail.com>
2026-03-20 18:51:46 -0400

    Remove outdated KNN model files and update existing model

--- app/ml/training/knn/steps.py
+++ app/ml/training/knn/steps.py
@@ -3,9 +3,11 @@ import pandas as pd
 from pathlib import Path
 
 from sklearn.cluster import KMeans
+from sklearn.compose import ColumnTransformer
 from sklearn.discriminant_analysis import StandardScaler
 from sklearn.neighbors import NearestNeighbors
 from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
 
 from app.domain.ml.base_model import BaseMLModel
 from app.domain.ml.base_step import BaseTrainingStep
@@ -15,18 +17,17 @@ from app.domain.ml.model_registry import model_registry
 from app.ml.models.knn_model import KnnModel
 
 BASE_DIR = Path(__file__).resolve().parents[4]  
-DATA_PATH = BASE_DIR / "storage" / "data" / "base.csv"
+DATA_PATH = BASE_DIR / "storage" / "data" / "consulta_base.csv"
 MODEL_PATH_BASE = BASE_DIR / "storage" / "models" 
 
 class LoadDataStep(BaseTrainingStep[TrainingContext]):
     def execute(self, ctx: TrainingContext) -> TrainingContext:
         df = pd.read_csv(DATA_PATH)
         print("Dataset original")
-        print(df.head())
         df.info()
-        
+        print(df.head())
+
         ctx.raw_data = df
-        ctx.extra["cod_cliente"] = 14111
         return ctx
     
     
@@ -45,10 +46,6 @@ class AddDerivedFeatureStep(BaseTrainingStep[TrainingContext]):
         df['DiasEntreCompras'] = (df['FechaVenta'] - df['FechaAnterior']).dt.days
         df['PromedioHistorico'] = df.groupby(['ID_Cliente', 'Producto'])['CantidadVendida'].transform('mean')
 
-        # Promedio móvil y Recencia
-        df['PromedioUltimas3'] = (df.groupby(['ID_Cliente', 'Producto'])['CantidadVendida']
-                                .rolling(window=3, min_periods=1).mean().reset_index(level=[0,1], drop=True))
-
         fecha_maxima = df['FechaVenta'].max()
         ultima_compra = df.groupby(['ID_Cliente', 'Producto'])['FechaVenta'].transform('max')
         df['DiasDesdeUltimaCompra'] = (fecha_maxima - ultima_compra).dt.days
@@ -60,49 +57,73 @@ class AddDerivedFeatureStep(BaseTrainingStep[TrainingContext]):
         # 6. Limpieza y Exportación
         df['DiasEntreCompras'] = df['DiasEntreCompras'].fillna(0)
 
-        ctx.clean_data = df
+        ctx.raw_data = df
+        print("Dataset con features derivados:")
+        print(df.head())
         return ctx
     
         
 class CleanColumnsStep(BaseTrainingStep[TrainingContext]):
     def execute(self, ctx: TrainingContext) -> TrainingContext:
-        if ctx.clean_data is None:
-            ctx.errors.append("CleanDataStep: clean_data es None, CleanColumnsStep no se ejecutó.")
+        if ctx.raw_data is None:
+            ctx.errors.append("CleanDataStep: raw_data es None, CleanColumnsStep no se ejecutó.")
             return ctx
         
-        df = ctx.clean_data.copy()   
+        df = ctx.raw_data.copy()   
         df_limpio = df.dropna(subset=['ID_Cliente', 'Producto', 'CantidadVendida'])
+        # df_limpio = df_limpio.dropna()
         df_limpio = df_limpio[df_limpio['CantidadVendida'] > 0]
-        df_limpio = df_limpio.dropna()
-
+        df_limpio = df_limpio.drop(
+            columns=[
+                # "Producto",
+                "Nombre_Ruta",
+                "Nombre_Zona",
+                "FechaAnterior",
+                "FechaVenta",
+                "Vendedor",
+            ]
+        )
         ctx.clean_data = df_limpio
+        print("Dataset limpio:")
+        print(df_limpio.info())
+        print(df_limpio.head())
         return ctx
     
-class TransformColumnsStep(BaseTrainingStep[TrainingContext]):
+class SegmentCustomersStep(BaseTrainingStep[TrainingContext]):
     def execute(self, ctx: TrainingContext) -> TrainingContext:
         if ctx.clean_data is None:
             ctx.errors.append("CleanDataStep: clean_data es None, TransformColumnsStep no se ejecutó.")
             return ctx
         
         df = ctx.clean_data.copy()   
-        df['FechaVenta'] = pd.to_datetime(df['FechaVenta'], errors='coerce')
         perfil_clientes = df.groupby('ID_Cliente').agg({
                 'CantidadVendida': 'sum',      # Volumen total
                 'PromedioHistorico': 'mean',   # Tamaño de pedido habitual
                 'DiasEntreCompras': 'mean',    # Frecuencia de visita
                 'Mes': 'nunique'               # Constancia a lo largo del año
             }).reset_index().fillna(0)
-        
+      
         scaler = StandardScaler()
         features_to_scale = ['CantidadVendida', 'PromedioHistorico', 'DiasEntreCompras', 'Mes']
         X_scaled = scaler.fit_transform(perfil_clientes[features_to_scale])
         
         kmeans = KMeans(n_clusters=5, init='k-means++', random_state=42, n_init=10)
-        perfil_clientes['Cluster'] = kmeans.fit_predict(X_scaled)
+        segmento = kmeans.fit_predict(X_scaled)
+        perfil_clientes['Cluster'] = segmento
+        df = df.merge(
+            perfil_clientes[['ID_Cliente', 'Cluster']],
+            on='ID_Cliente',
+            how='left'
+        )
         
         ctx.extra["x_scaled"] = X_scaled
         ctx.extra["perfil_clientes"] = perfil_clientes
         ctx.clean_data = df
+        
+        print("Datos Segmentados:")
+        print(perfil_clientes.head())
+        print(df.head())
+        print(df.info())
         return ctx
     
 
@@ -113,8 +134,36 @@ class SaveModelStep(BaseTrainingStep[TrainingContext]):
             ctx.errors.append("CleanDataStep: clean_data es None, StandarizationStep no se ejecutó.")
             return ctx
         
-        df = ctx.extra["x_scaled"].copy()   
+        cat_features = [
+            "LineaProducto",
+            "Marca",
+            "ClasificacionCliente",
+            "LineaProducto",
+            "Sucursal",
+        ]
+
+        num_features = [
+            "ID_Ruta",
+            "ID_Zona",
+            "CantidadVendida",
+            "PromedioHistorico",
+            "DiasEntreCompras",
+            "DiasDesdeUltimaCompra",
+            "Mes",
+            "Cluster"
+        ]
+        
+        preprocess = ColumnTransformer(
+            transformers=[
+                ("cat", OneHotEncoder(handle_unknown="ignore"), cat_features),
+                ("num", StandardScaler(), num_features)
+            ]
+        )
+
+        # df = ctx.extra["x_scaled"].copy()   
+        df = ctx.clean_data.copy()   
         model = Pipeline([
+            ("prep", preprocess),
             ("knn", NearestNeighbors(
                 n_neighbors=29,
                 metric="cosine"
@@ -127,7 +176,7 @@ class SaveModelStep(BaseTrainingStep[TrainingContext]):
         joblib.dump(
             {
                 "model": model,
-                "df": df
+                "df": ctx.clean_data
             },
             str(path_model)
         )
@@ -139,7 +188,7 @@ class RegistryModelStep(BaseTrainingStep[TrainingContext]):
     def execute(self, ctx: TrainingContext) -> TrainingContext:
         path_model = str(ctx.extra.get("path_model"))
    
-        meta_data = ModelMetadata(name=ctx.model_name, version=ctx.version, path_model=path_model,extra=ctx.extra)
+        meta_data = ModelMetadata(name=ctx.model_name, version=ctx.version, path_model=path_model,extra=ctx.extra) #sacar el extra de aqui y guardar en el archivo pkl
         model = KnnModel(metadata=meta_data)
         model.load(path_model)
         
