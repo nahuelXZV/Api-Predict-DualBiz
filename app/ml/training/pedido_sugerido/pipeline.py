5063c6b91565f83bf7f2e7edc44af30c89fc0f22
Heidy Olmos Campos <heidy.olmos@dualbiz.net>
2026-03-27 19:05:14 -0400

    ADD - Apriori step

--- app/ml/training/pedido_sugerido/pipeline.py
+++ app/ml/training/pedido_sugerido/pipeline.py
@@ -1,14 +1,15 @@
 from app.domain.ml.base_pipeline import BasePipeline
 from app.ml.training.pedido_sugerido.steps import (
-    CalculatedFeatureStep,
-    CleanDataStep,
-    KnnStep,
+    CalculoAtributosDerivadosStep,
+    EDA_CleanDataStep,
+    VecinosCercanos_KnnStep,
     LoadDataStep,
     PrepareDataXGBStep,
     RegistryModelStep,
     SaveModelStep,
-    KMeansStep,
-    TrainCantidadStep,
+    Clustering_KMeansStep,
+    EnsembleArboles_XGBoostStep,
+    ConjuntoReglasApriori_Step,
 )
 
 
@@ -16,12 +17,13 @@ def build_pedido_sugerido_pipeline() -> BasePipeline:
     pipeline = BasePipeline()
 
     pipeline.add_step(LoadDataStep())
-    pipeline.add_step(CleanDataStep())
-    pipeline.add_step(CalculatedFeatureStep())
-    pipeline.add_step(KMeansStep())
-    pipeline.add_step(KnnStep())
+    pipeline.add_step(EDA_CleanDataStep())
+    pipeline.add_step(CalculoAtributosDerivadosStep())
+    pipeline.add_step(Clustering_KMeansStep())
+    pipeline.add_step(VecinosCercanos_KnnStep())
+    pipeline.add_step(ConjuntoReglasApriori_Step())
     pipeline.add_step(PrepareDataXGBStep())
-    pipeline.add_step(TrainCantidadStep())
+    pipeline.add_step(EnsembleArboles_XGBoostStep())
     pipeline.add_step(SaveModelStep())
     pipeline.add_step(RegistryModelStep())
 
